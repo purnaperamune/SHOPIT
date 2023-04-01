372d0df393313ff392a516da3f688782fbad9060
Purna Peramune <purnaperamune@gmail.com>
2023-04-01 16:00:27 +0530

    Implemented more backend routes.
    Implemented routes to,
    Create new products
    Get product details,
    Get product details by id,
    Update product details,
    Delete product details.
    
    Also created a seeder to delete and insert all product at once.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,85 @@
-exports.getProducts = (req, res, next) => {
+const Product = require('../models/products')
+
+exports.newProduct = async (req, res, next) => {
+    const product = await Product.create(req.body);
+
+    res.status(201).json({
+        success: true,
+        product
+    })
+}
+
+
+
+exports.getProducts = async (req, res, next) => {
+    const products = await Product.find();
+
     res.status(200).json({
         success: true, 
-        message: 'This route will show all products in database.'
+        count: products.length,
+        products
     })
+}
+
+// Get single product details   =>   /api/v1/product/:id
+exports.getSingleProduct = async (req, res, next) => {
+
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: "Product not found!"
+        })
+    }
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+
+// Update Product   =>   /api/v1/admin/product/:id
+exports.updateProduct = async (req, res, next) => {
+
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: "Product not found!"
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+    });
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+
+}
+
+// Delete Product   =>   /api/v1/admin/product/:id
+exports.deleteProduct = async (req, res, next) => {
+
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: "Product not found!"
+        })
+    }
+
+    await product.deleteOne();
+
+    res.status(200).json({
+        success: true,
+        message: 'Product is deleted.'
+    })
+
 }
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router(); 
 
-const {getProducts} = require('../controllers/productController');
+const {getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct} = require('../controllers/productController');
 
 router.route('/products').get(getProducts);
+router.route('/product/:id').get(getSingleProduct); 
+router.route('/admin/product/:id').put(updateProduct); 
+router.route('/admin/product/:id').delete(deleteProduct); 
+router.route('/admin/product/new').post(newProduct);
 
-module.exports = router;
+module.exports = router; 
