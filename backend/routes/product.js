c218af5c1b1cfbaec1be89a8e828d45b2e64b8c6
Purna Peramune <purnaperamune@gmail.com>
2023-04-02 17:28:27 +0530

    User registration and authentications
    User registration endpoints and authentications with the use of JWT token.

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router(); 
 
 const {getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct} = require('../controllers/productController');
+const { isAuthenticatedUser} = require('../middlewares/auth')
 
-router.route('/products').get(getProducts);
+router.route('/products').get(isAuthenticatedUser, getProducts);
 router.route('/product/:id').get(getSingleProduct); 
 router.route('/admin/product/:id').put(updateProduct); 
 router.route('/admin/product/:id').delete(deleteProduct); 
