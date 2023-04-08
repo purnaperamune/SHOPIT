aae4803dc243caf4192c26250438b6c4d675a20a
Purna Peramune <purnaperamune@gmail.com>
2023-04-08 20:52:35 +0530

    Endpoints related to reviews

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router(); 
 
-const {getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct} = require('../controllers/productController');
+const {getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct, createProductReview, getProductReviews, deleteReview} = require('../controllers/productController');
 const { isAuthenticatedUser, authorizeRoles} = require('../middlewares/auth')
 
 router.route('/products').get(getProducts);
@@ -10,5 +10,9 @@ router.route('/product/:id').get(getSingleProduct);
 router.route('/admin/product/:id').put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct); 
 router.route('/admin/product/:id').delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct); 
 router.route('/admin/product/new').post(isAuthenticatedUser, authorizeRoles('admin'), newProduct);
+
+router.route('/review').put(isAuthenticatedUser, createProductReview)
+router.route('/reviews').get(isAuthenticatedUser, getProductReviews)
+router.route('/reviews').delete(isAuthenticatedUser, deleteReview)
 
 module.exports = router; 
