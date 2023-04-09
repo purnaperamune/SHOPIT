e450c8a7ea7a27ecb707310679d6e1906ef67c5b
Purna Peramune <purnaperamune@gmail.com>
2023-04-09 16:34:42 +0530

    React routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/layout/header'
 import Footer from './components/layout/Footer'
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container container-fluid"> 
+          <Routes>
+            <Route path="/" component={Home} exact />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+    
   );
 }
 
